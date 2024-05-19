176ca56a6d10bfd9b48cc6b04bf77ab9cd137b5e
arthurriosb <artyriosb@users.noreply.github.com>
2024-05-19 17:34:55 -0300

    feat: creates Hero component UI

---
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -0,0 +1,81 @@
+import { Container, Grid, Typography, styled } from "@mui/material";
+import Avatar from "../../../../assets/images/avatar.png";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
+import EmailIcon from "@mui/icons-material/Email";
+import SteyledButton from "../../../../components/StyledButton/StyledButton";
+
+const Hero = () => {
+
+  const StyledHero = styled("div")(({ theme }) => ({
+    backgroundColor: theme.palette.primary.main,
+    display: "flex",
+    alignItems: "center",
+    height: "100vh",
+  }))
+
+  const StyledImg = styled("img")(({ theme }) => ({
+    width: "80%",
+    borderRadius: "50%",
+    border: `1px solid ${theme.palette.primary.contrastText}`,
+  }));
+
+  return (
+    <>
+      <StyledHero>
+        <Container maxWidth="lg">
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={5}>
+              <StyledImg src={Avatar} />
+            </Grid>
+            <Grid item xs={12} md={7}>
+              <Typography
+                color="primary.contrastText"
+                variant="h1"
+                textAlign="center"
+                fontWeight="400"
+                pb={2}
+              >
+                Arthur Rios
+              </Typography>
+              <Typography
+                color="primary.contrastText"
+                variant="h2"
+                textAlign="center"
+              >
+                I'm a Software Developer!
+              </Typography>
+              <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
+                <Grid
+                  item
+                  xs={12}
+                  md={4}
+                  display="flex"
+                  justifyContent="center"
+                >
+                  <SteyledButton>
+                    <FileDownloadIcon />
+                    <Typography>Download CV</Typography>
+                  </SteyledButton>
+                </Grid>
+                <Grid
+                  item
+                  xs={12}
+                  md={4}
+                  display="flex"
+                  justifyContent="center"
+                >
+                  <SteyledButton>
+                    <EmailIcon />
+                    <Typography>Contact me</Typography>
+                  </SteyledButton>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Container>
+      </StyledHero>
+    </>
+  );
+};
+
+export default Hero;
